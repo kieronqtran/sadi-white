9363d576a360107b32ae0f22950457ffe18328a3
Shiro Guo <s3634953@rmit.edu.vn>
2017-11-30 19:46:30 +0700

    It's not working =)))) yet again :))) Please do something, like pretty pleaseeeeeee

--- src/main/webapp/src/reducers/reducers.js
+++ src/main/webapp/src/reducers/reducers.js
@@ -1,6 +1,7 @@
-import { combineReducers } from 'redux'
-import { reducer as formReducer } from 'redux-form'
-import authReducer from './auth'
+import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import authReducer from './auth';
+import getTestList from './test-reducer.js';
 
 function userReducer(state = {}, action) {
   switch (action.type) {
@@ -27,6 +28,7 @@ const combinedReducer = combineReducers({
   userReducer,
   form: formReducer,
   auth: authReducer,
-})
+  test: getTestList,
+});
 
 export default combinedReducer
