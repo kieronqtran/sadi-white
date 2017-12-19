19e80a0fc7d13af08acfb876137be9dd3bde7785
Edward Newgate <badaodaica1991@gmail.com>
2017-12-19 12:23:55 +0700

    Fix the Cookies and Order the All Results

--- src/main/webapp/src/actions/testing-restful.js
+++ src/main/webapp/src/actions/testing-restful.js
@@ -44,6 +44,29 @@ export function getTestById(id) {
   }
 }
 
+function setCookie(cname,cvalue,extime){
+  var d = new Date();
+  d.setTime(d.getTime()+(extime*1000));
+  var expire = "expires=" + d.toUTCString();
+  document.cookie = cname +"=" + cvalue + ";" + expire + ";path=/";
+}
+
+function getCookie(cname){
+  var name = cname + "=";
+  var decodedCookie= decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for (var i=0; i<ca.length; i++){
+    var c = ca[i];
+    while (c.charAt(0) == ' '){
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0){
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 export function getListTest(){
 	return async dispatch => {
     const token = docCookies.getItem('token')
--- src/main/webapp/src/reducers/allResult.js
+++ src/main/webapp/src/reducers/allResult.js
@@ -5,7 +5,9 @@ import {
 export default function allResultReducer(state = [], action) {
   switch (action.type) {
     case GET_ALL_RESULT_SUCCESSFUL:
-      state = action.allResults
+      state = action.allResults.sort(function (a,b) {
+         return ((b.numberOfCorrectAnswer/b.size)-(a.numberOfCorrectAnswer/a.size))
+      })
       return state
     default:
       return state
