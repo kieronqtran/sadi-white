9a18b00571f8654683c4954a213816b797eb365b
Edward Newgate <badaodaica1991@gmail.com>
2017-12-21 13:43:09 +0700

    Mail Service done

---
+++ src/main/java/sadi/whitegroup/assignment1/service/MailService.java
@@ -0,0 +1,78 @@
+package sadi.whitegroup.assignment1.service;
+
+import org.apache.commons.lang3.CharEncoding;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import sadi.whitegroup.assignment1.entity.User;
+
+import javax.mail.internet.MimeMessage;
+import java.util.Locale;
+
+@Service
+public class MailService {
+
+    private final Logger log = LoggerFactory.getLogger(MailService.class);
+
+    private static final String USER = "user";
+    private static final String BASE_URL = "baseUrl";
+
+    private final JavaMailSender javaMailSender;
+    private final MessageSource messageSource;
+    private final SpringTemplateEngine templateEngine;
+
+    public MailService(JavaMailSender javaMailSender,
+                       MessageSource messageSource, SpringTemplateEngine templateEngine) {
+        this.javaMailSender = javaMailSender;
+        this.messageSource = messageSource;
+        this.templateEngine = templateEngine;
+    }
+
+    @Async
+    public void sendEmail(String to, String content,
+                          boolean isMultipart, boolean isHtml) {
+        log.debug("Send email[multipart '{}' and html '{}'] to '{}' with subject '{}' and content={}",
+            isMultipart, isHtml, to, content);
+
+        // Prepare message using a Spring helper
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, isMultipart, CharEncoding.UTF_8);
+            message.setTo(to);
+            message.setFrom("Admin@localhost");
+            message.setText(content, isHtml);
+            javaMailSender.send(mimeMessage);
+            log.debug("Sent email to User '{}'", to);
+        } catch (Exception e) {
+            if (log.isDebugEnabled()) {
+                log.warn("Email could not be sent to user '{}'", to, e);
+            }
+            else {
+                log.warn("Email could not be sent to user '{}': {}", to, e.getMessage());
+            }
+        }
+    }
+
+    @Async
+    public void sendEmailFromTemplate(User user, String templateName) {
+        Context context = new Context();
+        context.setVariable(USER, user);
+        context.setVariable(BASE_URL, "http://127.0.0.1:8080");
+        String content = templateEngine.process(templateName, context);
+        sendEmail(user.getEmail(), content, false, true);
+
+    }
+
+    @Async
+    public void sendCreationEmail(User user) {
+        log.debug("Sending creation email to '{}'", user.getEmail());
+        sendEmailFromTemplate(user, "creationEmail");
+    }
+
+}
